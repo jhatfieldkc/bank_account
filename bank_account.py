9de9e37ce95d5f3866ec9577b1106eaf0955daf2
Jeremy Hatfield <jhatfieldkc@gmail.com>
2021-06-08 15:31:34 -0500

    update to chained methods

--- bank_account.py
+++ bank_account.py
@@ -7,12 +7,15 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
     
     def make_withdrawl(self, amount):
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self,):
         print(f"User: {self.name},", f"Balance: ${self.account_balance}")
+        return self
 
     # def transfer_money(self, other_user, amount):
     #     # self.account_balance-= amount
@@ -24,23 +27,10 @@ guido = User("Guido van Rossum", "guido@python.com")
 monty = User("Monty Python", "monty@python.com")
 jeremy=User("Jeremy Hatfield", "ceo@world.com")
 
-guido.make_deposit(100)
-guido.make_deposit(200)
-guido.make_deposit(50)
-guido.make_withdrawl(100)
-guido.display_user_balance()
+guido.make_deposit(100).make_deposit(200).make_deposit(50).make_withdrawl(100).display_user_balance()
 
+monty.make_deposit(500).make_deposit(50).make_withdrawl(100).make_withdrawl(100).display_user_balance()
 
-monty.make_deposit(50)
-monty.make_deposit(50)
-monty.make_withdrawl(100)
-monty.make_withdrawl(100)
-monty.display_user_balance()
-
-jeremy.make_deposit(150000000000)
-jeremy.make_withdrawl(100)
-jeremy.make_withdrawl(1000)
-jeremy.make_withdrawl(10000)
-jeremy.display_user_balance()
+jeremy.make_deposit(150000000000).make_withdrawl(100).make_withdrawl(1000).make_withdrawl(10000).display_user_balance()
 
 # jeremy.transfer_money(guido, 100)
